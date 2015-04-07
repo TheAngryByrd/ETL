b7ef90bcacc182376cfd517c48c1d88770cff045
Jimmy Byrd <jimmybyrd@gmail.com>
2015-04-07 19:38:37 -0400

    Added test proj

---
+++ ETL/ETL.Tests/Library1.fs
@@ -0,0 +1,11 @@
+﻿namespace ETL.Tests
+
+module Tests =
+
+    open FsCheck
+    open Xunit
+
+    [<Fact>]
+    let ``Reverse of reverse of a list is the original list``() =
+      let revRevIsOrig (xs:list<int>) = List.rev(List.rev xs) = xs
+      Check.QuickThrowOnFailure revRevIsOrig 
