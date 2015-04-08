b470ae6cd3f887e34a6fcd1ac7f8cef2c69ab2b1
Jimmy Byrd <jimmybyrd@gmail.com>
2015-04-08 07:27:47 -0400

    Using Chessie pattern match in subscribe

--- ETL/ETL/Program.fs
+++ ETL/ETL/Program.fs
@@ -4,25 +4,35 @@ open System.Reactive.Linq
 open System.Reactive.Threading.Tasks
 open System.Threading
 open Loaders
+open Chessie.ErrorHandling
 
 
 module Observable =
+    let ofAsync async =    
+        let task = async |> Async.StartAsTask
+        task.ToObservable()
 
 
 let batcher = new BatchProcessingAgent<string>(10000, 1000)
 let stream = Console.OpenStandardOutput()
-let batchProducer =batcher.BatchProduced :> IObservable<_> 
-batchProducer
+
+batcher.BatchProduced
     .SelectMany(fun items -> sendManyViaStream stream items |> Observable.ofAsync)
     .SubscribeOn(System.Reactive.Concurrency.ThreadPoolScheduler.Instance)
     .ObserveOn(System.Reactive.Concurrency.ThreadPoolScheduler.Instance)
-    .Add(fun result -> ())
+    .Add(fun result -> 
+            match result with 
+            | Pass result -> ()
+            | Warn (result, messages) ->()
+            | Fail messages -> ()
+        )
 
 [<EntryPoint>]
 let main argv = 
     //printfn "%A" argv
     use cts = new CancellationTokenSource()
-    for i in 1..1000000 do
+        
+    for i in 1..100000 do
         i.ToString() + "\n" |> batcher.Enqueue
     Console.ReadLine() |> ignore
     0 // return an integer exit code
